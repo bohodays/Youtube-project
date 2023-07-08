690ba2900f559a9159fea7544aaeba0e15806950
bohodays <bohodays0316@gmail.com>
2023-07-09 01:44:53 +0900

    feat : 비디오 카드 만들기 - Mock 데이터

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,10 +1,36 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import { useParams } from "react-router-dom";
+import VideoCard from "../components/VideoCard";
 
 const Videos = () => {
   const { keyword } = useParams();
+  const {
+    isLoading,
+    error,
+    data: videos,
+  } = useQuery(["videos", keyword], async () => {
+    return fetch(`/videos/${keyword ? "search" : "popular"}.json`)
+      .then((res) => res.json())
+      .then((data) => data.items);
+  });
 
-  return <div>Videos {keyword ? `🔍${keyword}` : "❤"}</div>;
+  console.log(videos);
+
+  return (
+    <>
+      <div>Videos {keyword ? `🔍${keyword}` : "❤"}</div>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Something is wrong 😥</p>}
+      {videos && (
+        <ul>
+          {videos.map((video) => (
+            <VideoCard key={video.id} video={video} />
+          ))}
+        </ul>
+      )}
+    </>
+  );
 };
 
 export default Videos;
