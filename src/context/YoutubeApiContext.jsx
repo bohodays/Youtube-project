9b2e6010d77b7562a4b9d6cdc7c10044ca6f6b2e
bohodays <bohodays0316@gmail.com>
2023-07-09 22:23:24 +0900

    design : 비디오 카드 만들기 - 스타일링

--- src/context/YoutubeApiContext.jsx
+++ src/context/YoutubeApiContext.jsx
@@ -1,10 +1,12 @@
 import { createContext, useContext } from "react";
 import FakeYoutebeClient from "../api/fakeYoutubeClient";
 import Youtube from "../api/youtube";
+import YoutebeClient from "../api/youtubeClient";
 
 export const YoutubeApiContext = createContext();
 
 const client = new FakeYoutebeClient();
+// const client = new YoutebeClient();
 const youtube = new Youtube(client);
 
 export function YoutubeApiProvider({ children }) {
